26a7a9c348a9150371acae1346e0baf62848310b
don-v <54256066+don-v@users.noreply.github.com>
2021-11-07 08:50:38 -0500

    FG: 09_07: got ellipse, started hyperbola!

--- FunctionsNGraphs/Ch7/Section7_9/x09_07_polar_conics.py
+++ FunctionsNGraphs/Ch7/Section7_9/x09_07_polar_conics.py
@@ -23,7 +23,7 @@ def get_majaxis_coords(majaxis_dict:dict):
         points[i] = (majaxis_dict[k], k)
     return points   
 
-def polar_conic(_de, s, t, pos=True, cos=True):
+def polar_conic(_de, s, t, pos=True, cos=True, plot=False):
     """
     Let F be a fixed point and l a fixed line in a plane. The
     set of all points P in the plane such that the ratio d(P,F):
@@ -89,7 +89,23 @@ def polar_conic(_de, s, t, pos=True, cos=True):
             b = ((a**2) - (c**2))**(1/2)
             asq = a**2
             bsq = b**2
-            centerx = x1 + a if x1 < x2 else x1 - a
+            h = x1 + a if x1 < x2 else x1 - a
+
+            if plot:
+                do_ellipse(asq,bsq,h=h)
+            
+
+            print(f'''
+            a = {a:>9.2f}
+            b = {b:>9.2f}
+            c = {c:>9.2f}
+            e = {e:>9.2f}
+            asq = {asq:>9.2f}
+            bsq = {bsq:>9.2f}
+            h = {h:>9.2f}
+            ''')
+
+            return a,b,c,e,asq,bsq,h
 
         
         else:
@@ -105,8 +121,89 @@ def polar_conic(_de, s, t, pos=True, cos=True):
             a = dist/2
             c = a*e 
             b = ((a**2) - (c**2))**(1/2)
+            asq = b**2
+            bsq = a**2
+            k = y1 + a if y1 < y2 else y1 - a
+
+            if plot:
+                do_ellipse(asq,bsq,k=k)
+            
+            print(f'''
+            a = {a:>9.2f}
+            b = {b:>9.2f}
+            c = {c:>9.2f}
+            e = {e:>9.2f}
+            asq = {asq:>9.2f}
+            bsq = {bsq:>9.2f}
+            k = {k:>9.2f}
+            ''')
+
+            return a,b,c,e,asq,bsq,k
+
+    if type == 'hyperbola':
+        majaxis = {}
+        if cos:
+            thetas = [0, pi]
+            for theta in thetas:
+                r = _de/(s + sign*t*np.cos(theta))
+                majaxis[theta] = r
+            
+            points_dict = get_majaxis_coords(majaxis)
+            x1,y1 = convert_polar_to_rect(*points_dict[0])
+            x2,y2 = convert_polar_to_rect(*points_dict[1])
+            dist = distance(points_dict[0],points_dict[1])
+            a = dist/2
+            c = a*e 
+            b = ((c**2) - (a**2))**(1/2)
             asq = a**2
             bsq = b**2
-            centery = y1 + a if y1 < y2 else y2 - a
+            h = x1 + a if x1 < x2 else x1 - a
 
-    
+            if plot:
+                do_hyperbola(asq,bsq,h=h)
+            
+
+            print(f'''
+            a = {a:>9.2f}
+            b = {b:>9.2f}
+            c = {c:>9.2f}
+            e = {e:>9.2f}
+            asq = {asq:>9.2f}
+            bsq = {bsq:>9.2f}
+            h = {h:>9.2f}
+            ''')
+
+            return a,b,c,e,asq,bsq,h
+
+        
+        else:
+            thetas = [pi/2, (3*pi)/2]
+            for theta in thetas:
+                r = _de/(s + sign*t*np.sin(theta))
+                majaxis[theta] = r
+
+            points_dict = get_majaxis_coords(majaxis)
+            x1,y1 = convert_polar_to_rect(*points_dict[0])
+            x2,y2 = convert_polar_to_rect(*points_dict[1])
+            dist = distance(points_dict[0],points_dict[1])
+            a = dist/2
+            c = a*e 
+            b = ((c**2) - (a**2))**(1/2)
+            asq = a**2
+            bsq = b**2
+            k = y1 + a if y1 < y2 else y1 - a
+
+            if plot:
+                do_hyperbola(asq,bsq,k=k,horizontal=False)
+            
+            print(f'''
+            a = {a:>9.2f}
+            b = {b:>9.2f}
+            c = {c:>9.2f}
+            e = {e:>9.2f}
+            asq = {asq:>9.2f}
+            bsq = {bsq:>9.2f}
+            k = {k:>9.2f}
+            ''')
+
+            return a,b,c,e,asq,bsq,k
