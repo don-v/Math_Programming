5309b9d05b62c9a690d120711e6ac8866bd42244
don-v <54256066+don-v@users.noreply.github.com>
2021-11-27 11:07:41 -0500

    mp: 09_08 -- plane_curvs!

---
+++ FunctionsNGraphs/Ch7/Section7_9/x09_08_plane_curv.py
@@ -0,0 +1,22 @@
+from x09_07_polar_conics import *
+
+def theory_ex1(a,b, xexp:str, yexp:str):
+    points_dict = {}
+    for t in range(a,b+1):
+        x = eval(xexp) 
+        y = eval(yexp)
+        points_dict[t] = (x,y)
+    return points_dict
+
+def cube(x):
+    if 0<=x: return x**(1./3.)
+    return -(-x)**(1./3.)
+
+def theory_ex1_(a,b, xexp:str, yexp:str):
+    points_dict = {}
+    for t in range(a,b+1):
+        t = cube(t)
+        x = eval(xexp) 
+        y = eval(yexp)
+        points_dict[t] = (x,y)
+    return points_dict
