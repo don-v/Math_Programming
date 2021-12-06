2636aeeeeeb495630d67d974c9ca1e72d70c5e64
don-v <54256066+don-v@users.noreply.github.com>
2021-12-06 18:47:22 -0500

    mp: 09_08 -- ex23!

--- FunctionsNGraphs/Ch7/Section7_9/x09_08_plane_curv.py
+++ FunctionsNGraphs/Ch7/Section7_9/x09_08_plane_curv.py
@@ -51,3 +51,35 @@ def theory_ex18(a,b,xexp:str, yexp:str,n=50):
             y = eval(yexp)
             points_dict[t] = (x,y)
     return points_dict
+
+def theory_ex22(a,b,xexp:str, yexp:str,n=50):
+    points_dict = {}
+    for t in np.linspace(a,b,n):
+        if t > -pi/2 and t < pi/2:
+            x = eval(xexp) 
+            y = eval(yexp)
+            points_dict[t] = (x,y)
+    return points_dict
+
+C1 = ('t**2', 't')
+C2 = ('t**4', 't**2')
+C3 = ('(np.sin(t))**2', 'np.sin(t)')
+C4 = ('np.exp(2*t)'), '-1*(exp(t))'
+
+curves = [C1,C2,C3,C4]
+
+curve_points = {}
+for c in curves:
+    curve_points[c] = theory_ex3(-5,5,c[0],c[1],n=10)
+
+# for c in curve_points.items():
+#     print(c)
+
+def param_plot_nosho(points_dict):
+    for p in points_dict.values():
+        plt.plot(*p,marker='o')
+    return None 
+
+# for point_dict in curve_points.values():
+#     param_plot_nosho(point_dict)
+# plt.show()
