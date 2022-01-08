74ecb63a104416b8f5ba3c718be30dc5b4c9fd20
don-v <54256066+don-v@users.noreply.github.com>
2022-01-08 11:41:26 -0500

    mp: 09_09: started 27!

--- FunctionsNGraphs/Ch7/Section7_9/x09_09_review.py
+++ FunctionsNGraphs/Ch7/Section7_9/x09_09_review.py
@@ -5,13 +5,13 @@ from x09_08_plane_curv import *
 def rtheta26r(t1,t2, a=3,b=5,c=1,n=5):
     '''conchoid'''
     theta=np.linspace(t1,t2,n)
-    r = a*np.sin(b*theta)
+    r = a*np.cos(b*theta)
     r_theta=np.vstack((np.rad2deg(theta),r)).T
 
     table26r={}
-    for deg in range(0,375,15):
+    for deg in range(0,378,18):
         theta2 = np.deg2rad(deg)
-        r2 = a*np.sin(b*theta2)
+        r2 = a*np.cos(b*theta2)
         table26r[deg]={'r': f'{r2}', 'x,y': plot_rtheta(r2,theta2)}
 
     return r_theta, (r,theta), table26r 
@@ -30,16 +30,49 @@ def get_r26r(deg,a=3,b=5):
     return r
 
 
-# >>> def print_x0(t1,t2):
-# ...     x=rtheta26r(t1,t2)
-# ...     return x[0]
-# ... 
-# >>> def print_x1(t1,t2):   
-# ...     x=rtheta26r(t1,t2)
-# ...     return x[1] 
-# ... 
-# >>> def print_x2(t1,t2):   
-# ...     x=rtheta26r(t1,t2)
-# ...     return x[2] 
+def print_x0(t1,t2):
+    x=rtheta26r(t1,t2)
+    return x[0]
+ 
+def print_x1(t1,t2):   
+    x=rtheta26r(t1,t2)
+    return x[1] 
+ 
+def print_x2(t1,t2):   
+    x=rtheta26r(t1,t2)
+    for k,v in x[2].items():
+        print(f"{k:>5}: {eval(v['r']):>5.2f}")
+    return x[2] 
 
 # q26r end:
+
+
+# q27r start:
+
+def rtheta27r(t1,t2, a=6,b=3,c=1,n=5):
+    '''conchoid'''
+    theta=np.linspace(t1,t2,n)
+    r = a - b*np.cos(theta)
+    r_theta=np.vstack((np.rad2deg(theta),r)).T
+
+    table27r={}
+    for deg in range(0,375,15):
+        theta2 = np.deg2rad(deg)
+        r2 = a - b*np.cos(theta2)
+        table27r[deg]={'r': f'{r2}', 'x,y': plot_rtheta(r2,theta2)}
+
+    return r_theta, (r,theta), table27r 
+
+def plot27r(t1,t2,n=1000):
+    x = rtheta27r(t1,t2,n=n)
+    r = x[1][0]
+    theta = x[1][1]
+    plt.plot(*plot_rtheta(r,theta))
+    plt.show()
+    return None
+
+def prt27(t1,t2,n=5):
+    pass
+
+
+# q27r end:
