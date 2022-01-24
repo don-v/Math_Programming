93c26b1a6e51e8abd08a66936883f9bacf54d1e0
don-v <54256066+don-v@users.noreply.github.com>
2022-01-24 11:40:54 -0500

    mp: ch3 -- started building ch3 package!

---
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/__init__.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+
+# __all__ = ["x03_01_graph_polynomial"]
---
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_01_graph_polynomial.py
@@ -0,0 +1,13 @@
+"""
+A function f is a polynomial if:
+
+f(x) = a_{n}*x**n + a_{n-1}*x**(n-1) + ... + a_{1}*x + a_{0}
+
+where the coefficients a_{0}, a_{1}, ... , a_{n} are real numbers and
+the exponents are nonnegative integers
+
+"""
+
+if __name__ == '__main__':
+    pass
+
