ec055babd147bb425be3047b3da0a3e0f04c8005
don-v <54256066+don-v@users.noreply.github.com>
2022-01-25 23:28:44 -0500

    mp: x03_01 gen poly!

--- FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_01_graph_polynomial.py
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_01_graph_polynomial.py
@@ -7,7 +7,49 @@ where the coefficients a_{0}, a_{1}, ... , a_{n} are real numbers and
 the exponents are nonnegative integers
 
 """
+import sys, time
+
+
+def gen_coeffs(n=2):
+    coeffs = dict()
+    for x in range(n+1):
+        coeffs[f'a{n}'] = int(input(f'the value of a{n} is: '))
+    return coeffs
+
+def gen_poly(n=2):
+    if n==0: return f"c[f'a{n}']*x**{n}"
+    else: return f"c[f'a{n}']*x**{n}" + ' + ' + gen_poly(n-1)
+   
+def eval_poly(p:str, c:dict, rangex:tuple):
+    points = dict()
+    for x in range(*rangex):
+        points[x] = eval(p)
+    return points
+    
 
 if __name__ == '__main__':
-    pass
+    start = time.perf_counter()
+    if len(sys.argv) == 1:
+        c = gen_coeffs()
+        p = gen_poly()
+        points = eval_poly(p,c,(-5,6))
+        elapsed_time = time.perf_counter() - start
+        print(f"""
+        n={2}
+        polynomial={p}
+        elapsed time = {elapsed_time}
+        points = {points}
+        """)
+    else:
+        n = int(sys.argv[1])
+        c = gen_coeffs(n)
+        p = gen_poly(n)
+        points = eval_poly(p,c,(-5,6))
+        elapsed_time = time.perf_counter() - start
+        print(f"""
+        n={n}
+        polynomial={p}
+        elapsed time = {elapsed_time}
+        points = {points}
+        """)
 
