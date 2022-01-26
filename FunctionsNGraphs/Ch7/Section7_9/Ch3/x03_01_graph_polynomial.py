f42457bbac5ca645a353c3cfe9a8a2a94ca1579d
don-v <54256066+don-v@users.noreply.github.com>
2022-01-26 18:47:59 -0500

    mp: x03_01 display, plot poly!

--- FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_01_graph_polynomial.py
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_01_graph_polynomial.py
@@ -7,49 +7,56 @@ where the coefficients a_{0}, a_{1}, ... , a_{n} are real numbers and
 the exponents are nonnegative integers
 
 """
-import sys, time
+import sys
+import matplotlib.pyplot as plt
 
 
 def gen_coeffs(n=2):
     coeffs = dict()
     for x in range(n+1):
-        coeffs[f'a{n}'] = int(input(f'the value of a{n} is: '))
+        coeffs[f'a{x}'] = float(input(f'the value of a{x} is: '))
     return coeffs
 
 def gen_poly(n=2):
     if n==0: return f"c[f'a{n}']*x**{n}"
     else: return f"c[f'a{n}']*x**{n}" + ' + ' + gen_poly(n-1)
+
+def poly_display(c:dict, n=2):
+    if n==0: return f"{c[f'a{n}']:.2f}*x**{n}"
+    else: return f"{c[f'a{n}']:.2f}*x**{n}" + ' + ' + poly_display(c,n-1)
    
 def eval_poly(p:str, c:dict, rangex:tuple):
     points = dict()
     for x in range(*rangex):
         points[x] = eval(p)
     return points
-    
+
+def plot_poly(c:dict):
+    plt.plot(*zip(*sorted(c.items()))); plt.show()
+    return None    
 
 if __name__ == '__main__':
-    start = time.perf_counter()
     if len(sys.argv) == 1:
         c = gen_coeffs()
         p = gen_poly()
+        d = poly_display(c)
         points = eval_poly(p,c,(-5,6))
-        elapsed_time = time.perf_counter() - start
         print(f"""
         n={2}
-        polynomial={p}
-        elapsed time = {elapsed_time}
+        polynomial={d}
         points = {points}
         """)
+        plot_poly(points)
     else:
         n = int(sys.argv[1])
         c = gen_coeffs(n)
         p = gen_poly(n)
+        d = poly_display(c,n)
         points = eval_poly(p,c,(-5,6))
-        elapsed_time = time.perf_counter() - start
         print(f"""
         n={n}
-        polynomial={p}
-        elapsed time = {elapsed_time}
+        polynomial={d}
         points = {points}
         """)
+        plot_poly(points)
 
