ce4e1a1fbdb6bfafdb2bd0b21a5e80c03818626d
don-v <54256066+don-v@users.noreply.github.com>
2022-01-27 14:12:05 -0500

    mp: x03_01 ax_tothe_n!

--- FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_01_graph_polynomial.py
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_01_graph_polynomial.py
@@ -8,6 +8,7 @@ the exponents are nonnegative integers
 
 """
 import sys
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -25,15 +26,39 @@ def poly_display(c:dict, n=2):
     if n==0: return f"{c[f'a{n}']:.2f}*x**{n}"
     else: return f"{c[f'a{n}']:.2f}*x**{n}" + ' + ' + poly_display(c,n-1)
    
-def eval_poly(p:str, c:dict, rangex:tuple):
+def eval_poly(p:str, c:dict, rangex:tuple, lst=None):
     points = dict()
-    for x in range(*rangex):
-        points[x] = eval(p)
-    return points
+    if not lst:
+        for x in range(*rangex):
+            points[x] = eval(p)
+        return points
+    else:
+        for x in lst:
+            points[x] = eval(p)
+        return points
 
 def plot_poly(c:dict):
     plt.plot(*zip(*sorted(c.items()))); plt.show()
     return None    
+
+def ax_tothe_n(a,n):
+    x=np.linspace(-5,5,1000)
+    y=a*(x**n)
+    plt.plot(x,y)
+    return None
+
+def n4_ax_tothe_n(a=1,n_=10):
+    for i in range(n_):
+        ax_tothe_n(a,i)
+    plt.show()
+    return None
+
+def a4_ax_tothe_n(a=10,n_=3):
+    for i in range(a):
+        ax_tothe_n(i,n_)
+    plt.show()
+    return None
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
