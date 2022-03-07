7511a384b3893526c5125fbd43488a364215b35b
don-v <54256066+don-v@users.noreply.github.com>
2022-03-07 14:49:38 -0500

    mp: 03 04 -- simplified complex frac func!

--- FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_04_complex.py
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_04_complex.py
@@ -32,24 +32,53 @@ def gen_complex():
     b = int(input('what is the value of b? '))
     return complex(a,b)
 
-def gen_two_complex():
+def gen_n_complex(n=2):
     """generate and return two complex numbers
     of the form `a + bi`, where 'a' and 'b' are 
     real numbers and i is defined such that 
     i**2 = -1"""
     cnums = list()
-    for i in range(2):
+    for i in range(n):
         print(f'get a and b for z{i+1}:')
         cnums.append(gen_complex())
     return cnums[0], cnums[1]
 
+def simplify_complex_fraction(numerator, denominator):
+    """converting a fraction in which the numerator and
+    denominator are represented by complex numbers to a
+    singular complex number of the form `a + bi`, where 
+    'a' and 'b' are real numbers and i is defined such 
+    that i**2 = -1"""
+    d_conjugate = denominator.conjugate()
+    complex_const = denominator*d_conjugate
+    const = complex_const.real
+    result = numerator*d_conjugate
+
+    s = f"""
+    
+        {result.real}                      {result.imag}       
+    -------------    +    -------------  i
+        {const}                     {const}"""
+
+    return (1/const)*result, s
 
 if __name__ == '__main__':
-    z1,z2 = gen_two_complex()
+    z1,z2 = gen_n_complex()
     print(f'z1: {z1}')
     print(f'z2: {z2}')
-    print(f'sum z1, z2: {add_complex(z1,z2)}')
-    print(f'difference z1, z2: {sub_complex(z1,z2)}')
-    print(f'product z1,z2: {mult_complex(z1,z2)}')
+    print(f'sum z1 + z2: {add_complex(z1,z2)}')
+    print(f'difference z1 - z2: {sub_complex(z1,z2)}')
+    print(f'product z1 * z2: {mult_complex(z1,z2)}')
+    f,s = simplify_complex_fraction(z1,z2)
+    print(f"""
+    fraction:
+    
+        {z1}
+    -------------
+        {z2}
+
+    simplified: 
+    
+    {s}""")
     
     
