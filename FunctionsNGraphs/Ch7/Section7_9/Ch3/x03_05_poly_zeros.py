093b9b366d971bd7534daa22dab3be23a4e2dccb
don-v <54256066+don-v@users.noreply.github.com>
2022-03-17 14:04:13 -0400

    mp: 03 05 -- updated descarte!

--- FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_05_poly_zeros.py
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_05_poly_zeros.py
@@ -13,19 +13,60 @@ def get_two(x,n):
         i = 0
         while i < len(x)-1:
             n_slice = x[i:i+n]
-            print(f'{i}:{n_slice}')
             doubles.append((n_slice[0],n_slice[1]))
             i += 1
-    return None
+    return doubles
+
+def check_tuple_sign_change(x:tuple):
+    """compares the signs of a 2-tuple, and returns
+    True if the signs are different, otherwise, returns
+    False"""
+    if is_pos(x[0]) == is_pos(x[1]):
+        return False
+    return True
 
 def get_descarte_signs(x: list) -> int:
     """takes non-zero coefficients of a polynomial 2
     at a time and determines if there is a sign change
     between the two coefficients"""
+    sign_changes = 0
+    doubles = get_two(x)
+    for double in doubles:
+        if check_tuple_sign_change(double):
+            sign_changes +=1
+    return sign_changes
+
+def get_nonzero_coeff_list(coeff_list, fx=True):
+    """calculate coefficients for f(x) and f(-x)"""
+    degree  = get_degree(coeff_list)
+    print('degree:',degree)
+    if degree % 2 == 0:
+        if fx:
+            print("This is for f(x)")
+            return coeff_list
+        else:
+            print("This is for f(-x)")
+            for i in range(len(coeff_list)):
+                if i % 2 != 0:
+                    coeff_list[i] = -1*coeff_list[i]
+            return coeff_list
+    else:
+        if fx:
+            print("This is for f(x)")
+            return coeff_list
+        else:
+            print("This is for f(-x)")
+            for i in range(len(coeff_list)):
+                if i % 2 == 0:
+                    coeff_list[i] = -1*coeff_list[i]
+            return coeff_list
+
+def get_even_idx(list):
     pass
 
 
 if __name__ == '__main__':
     x = [x for x in range(4)]
-    get_two(x,2)
+    doubles = get_two(x,2)
+    print(doubles)
     
