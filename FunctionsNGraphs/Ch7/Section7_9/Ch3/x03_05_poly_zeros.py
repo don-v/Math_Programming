e16a919d5ad0d667532810e8dfbd4d80dec3ab7b
don-v <54256066+don-v@users.noreply.github.com>
2022-03-18 21:16:45 -0400

    mp: 03 05 -- got descartes rule of signs!

--- FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_05_poly_zeros.py
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_05_poly_zeros.py
@@ -4,7 +4,7 @@ def is_pos(x):
     """returns a boolean value for testing if x is positive"""
     return x > 0
 
-def get_two(x,n):
+def get_two(x,n=2):
     """returns successive slices of a list of size n"""
     doubles = list()
     if len(x) < 2:
@@ -20,7 +20,7 @@ def get_two(x,n):
 def check_tuple_sign_change(x:tuple):
     """compares the signs of a 2-tuple, and returns
     True if the signs are different, otherwise, returns
-    False"""
+    False, all 2-tuple elements must be non-zero"""
     if is_pos(x[0]) == is_pos(x[1]):
         return False
     return True
@@ -36,8 +36,12 @@ def get_descarte_signs(x: list) -> int:
             sign_changes +=1
     return sign_changes
 
-def get_nonzero_coeff_list(coeff_list, fx=True):
-    """calculate coefficients for f(x) and f(-x)"""
+def fx_fnegx_coeff_list(coeff_list, fx=True):
+    """calculate coefficients for f(x) and f(-x), note,
+    the coeff list DOES include zero coefficients for
+    all polynomial terms; the returned coeff_list will 
+    need to be further processed to retain only non-zero 
+    coefficients for polynomial terms of degree > 0"""
     degree  = get_degree(coeff_list)
     print('degree:',degree)
     if degree % 2 == 0:
@@ -61,12 +65,35 @@ def get_nonzero_coeff_list(coeff_list, fx=True):
                     coeff_list[i] = -1*coeff_list[i]
             return coeff_list
 
-def get_even_idx(list):
-    pass
-
+def get_nonzero_coeff_list(x):
+    last_element = x.pop()
+    while 0 in x:
+        x.remove(0)
+    x.append(last_element)
+    return x
 
 if __name__ == '__main__':
-    x = [x for x in range(4)]
-    doubles = get_two(x,2)
-    print(doubles)
+    coeff_list_fx = [2,-7,0,3,6,-5]
+    coeff_list_fnegx = fx_fnegx_coeff_list(coeff_list_fx.copy(), False)
+    nz_coeff_list_fx = get_nonzero_coeff_list(coeff_list_fx.copy())
+    nz_coeff_list_fnegx = get_nonzero_coeff_list(coeff_list_fnegx.copy())
+    real_pos_roots_max = get_descarte_signs(nz_coeff_list_fx)
+    real_neg_roots_min = get_descarte_signs(nz_coeff_list_fnegx)
+    fx = poly_display_from_list(coeff_list_fx)
+    fnegx = poly_display_from_list(coeff_list_fnegx)
+    print(f"""
+    for the polynomial f(x): 
+        
+        {fx}
     
+    By Descartes' Rule of Signs:
+    there are at most {real_pos_roots_max} positive real roots.
+    
+    by evaluating f(-x), we get the polynomial: 
+
+        {fnegx}
+    
+    from the polynomial form of f(-x), we see, again by Descartes' Rule 
+    of Signs, that there are at most {real_neg_roots_min} negative 
+    real roots.    
+    """)
