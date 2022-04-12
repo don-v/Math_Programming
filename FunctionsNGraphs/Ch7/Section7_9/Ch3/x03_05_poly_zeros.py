5fda9e79ee5a19f36857a533fc49a87b37454af4
don-v <54256066+don-v@users.noreply.github.com>
2022-04-12 13:20:48 -0400

    mp: 03 05 -- updated main for poly from roots!

--- FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_05_poly_zeros.py
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_05_poly_zeros.py
@@ -194,39 +194,6 @@ def do_sec3_5_ex1_6(c1,c2,c3,x,fx):
     return coeff_list, a, final_coeffs, poly
 
 
-'''
-I'm studying pre-calculus, and in my mathbook there is the following theorem which is the basis for my question:
-
-If $f(x)$ is a polynomial of degree $n\space >\space 0$, then there exists n complex numbers, $c_1, c_2, ..., c_n$ such that:
-
-$$ f(x) = a(x-c_1)(x-c_2)\cdots(x-c_n)$$
-
-where $a$ is the leading coefficient of $f(x)$.  Each number $c_j$ is a zero of $f(x)$.
-
-Here's the question:
-
-   $c_1 = \sqrt{2},\space c_2=\pi,\space c3 = 0; f(0)=0$
-
-I need to find a polynomial of degree 3 with the given zeros $c_1,\space c_2,\space c_3$
-
-so when I plug in the zeros into the formula given in the theorem, I get:
-
-$$f(x) = a(x-\sqrt{2})(x-\pi)(x-0)$$
-
-$$f(x) = a(x-\sqrt{2})(x-\pi)(x)$$
-
-$$f(x) = a[(x^3+(-\sqrt{2}-\pi)(x^2)+\sqrt{2}\pi x]$$
-
-then evaluating our polynomial at $x=0:$
-
-$$f(0) = a[0] = 0$$
-
-Does this implies $a$ can be any real number?
-
-If so, would that imply 
-
-'''
-
 def get_binomial(a,b,n):
     '''returns the sum from k = 0 to n for 
     (n choose k)*a**(n-k)*b**k'''
@@ -258,15 +225,34 @@ def display_binomial(a,b,n):
 
 
 if __name__ == '__main__':
-    x = [x for x in range(1,6)]
-    x1 = [x*(-1)**n for n,x in enumerate(range(1,6))]
-    x2 = [x*(-1)**(n+1) for n,x in enumerate(range(1,6))]
 
-    xs = [x,x1,x2]
-    for e in xs:
-        print(f'list: {e}')
-        print(f'check upper: {check_poly_boundary(e)}')
-        print(f'check lower: {check_poly_boundary(e,upper=False)}')
+    c1 = eval(input("Input the first root: "))
+    c2 = eval(input("Input the 2nd root: "))
+    c3 = eval(input("Input the 3rd root: "))
+    x = eval(input("The value at which the polynomial is evaluated: "))
+    fx = eval(input("The value of the polynomial evaluated at 'x': "))
+    f = get_deg3_poly_from_roots
+    g = f(c1,c2,c3)
+    a = fx/eval_poly3(x,g)
+    final_coeffs = mult_coeffs(g,a)
+    poly = poly_display_from_list(final_coeffs)
+    print(f'''
+    g_coeffs: {g}
+    g(x): {poly_display_from_list(g)}
+    a: {a}
+    final_coeffs: {final_coeffs}
+    final_poly: {poly}
+    ''')
+
+    # x = [x for x in range(1,6)]
+    # x1 = [x*(-1)**n for n,x in enumerate(range(1,6))]
+    # x2 = [x*(-1)**(n+1) for n,x in enumerate(range(1,6))]
+
+    # xs = [x,x1,x2]
+    # for e in xs:
+    #     print(f'list: {e}')
+    #     print(f'check upper: {check_poly_boundary(e)}')
+    #     print(f'check lower: {check_poly_boundary(e,upper=False)}')
 
 
     # coeff_list_fx = [3,0,4,0,2,-5]
@@ -297,3 +283,36 @@ if __name__ == '__main__':
     # of Signs, that there are at most {real_neg_roots_max} negative 
     # real roots.    
     # """)
+
+    '''
+I'm studying pre-calculus, and in my mathbook there is the following theorem which is the basis for my question:
+
+If $f(x)$ is a polynomial of degree $n\space >\space 0$, then there exists n complex numbers, $c_1, c_2, ..., c_n$ such that:
+
+$$ f(x) = a(x-c_1)(x-c_2)\cdots(x-c_n)$$
+
+where $a$ is the leading coefficient of $f(x)$.  Each number $c_j$ is a zero of $f(x)$.
+
+Here's the question:
+
+   $c_1 = \sqrt{2},\space c_2=\pi,\space c3 = 0; f(0)=0$
+
+I need to find a polynomial of degree 3 with the given zeros $c_1,\space c_2,\space c_3$
+
+so when I plug in the zeros into the formula given in the theorem, I get:
+
+$$f(x) = a(x-\sqrt{2})(x-\pi)(x-0)$$
+
+$$f(x) = a(x-\sqrt{2})(x-\pi)(x)$$
+
+$$f(x) = a[(x^3+(-\sqrt{2}-\pi)(x^2)+\sqrt{2}\pi x]$$
+
+then evaluating our polynomial at $x=0:$
+
+$$f(0) = a[0] = 0$$
+
+Does this implies $a$ can be any real number?
+
+If so, would that imply 
+
+'''
