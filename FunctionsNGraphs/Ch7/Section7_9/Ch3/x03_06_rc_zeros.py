713c9cc6c8cbbe191fb913edefba6382bc29b3a8
don-v <54256066+don-v@users.noreply.github.com>
2022-06-15 17:49:22 -0400

    mp: 03 06 theory defs

--- FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_06_rc_zeros.py
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_06_rc_zeros.py
@@ -1,4 +1,29 @@
 from Ch3.x03_05_poly_zeros import *
 
+"""
+Theorem: If f(x) is a polynomial of degree n > 0
+and has real coefficients and if z is a complex
+zero of f(x), then the conjugate z-bar of z is
+also a zero of f(x)
+"""
+
+"""
+Theorem: Every polynomial with real coefficients and 
+positive degree n can be expressed as a product of 
+linear and quadratic polynomials with real coefficients 
+where the quadractic factors have no real zeros
+"""
+
+"""
+Theorem of Rational Zeros: Suppose that 
+f(x) = a_n*x**n + a_{n-1}*x**(n-1) + ...
++ a_1*x**1 + a_0 is a polynomial with
+integer coeffcients. If c/d is a rational
+zero of f(x), where c and d have no common
+prime factors and c > 0, then c is a factor
+of a_0 and d is a factor of a_n.
+"""
+
+
 if __name__ == '__main__':
     pass
