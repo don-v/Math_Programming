eff8385779e7384f91a0ab03fbacac491fbceacf
don-v <54256066+don-v@users.noreply.github.com>
2022-07-18 22:39:51 -0400

    mp: 03 07 -- started 03 07!

---
+++ FunctionsNGraphs/Ch7/Section7_9/Ch3/x03_07_rational_funcs.py
@@ -0,0 +1,24 @@
+from Ch3.x03_06_rc_zeros import *
+
+'''
+A function f is a **rational function** if, for all x in its
+domain:
+
+f(x) = g(x)/h(x)
+
+where g(x) and h(x) aer polynomials. Of majoer importance are
+the zeros of the numerator and denominator. _Throughout this
+section we shall assume that g(x) and h(x) have no common factors,
+and hence, no common zeros. 
+
+Note that f(c) = 0 if and only if g(c) = 0, and hence the zeros
+of the numerator g(x) are teh zeros of f(x).
+
+However, if c is a zero of the demonimator, h(x), then
+f(c) = g(c)/0 is undefined, and hence the behavior of f(x) 
+requires special attention when x is near c!
+'''
+
+
+if __name__ == '__main__':
+    pass
